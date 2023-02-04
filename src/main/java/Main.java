dd96bad5418b5907f4063bed6ece98c766ec9c18
neerajdeochake <neerajdeochake12@gmail.com>
2023-02-04 22:35:26 +0530

    Two sum leetcode

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+import leetcode.easy.TwoSum;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        TwoSum problem = new TwoSum();
+        problem.solution(new int[] {2, 7, 11, 15},9);
+    }
+}
---
+++ src/test/java/leetcode/easy/TwoSumTest.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+
+@RunWith(JUnit4.class)
+public class TwoSumTest {
+    private TwoSum ts;
+    private int[] result;
+
+
+    @Before
+    public void init(){
+        this.ts=new TwoSum();
+        this.result=new int[]{-1,-1};
+    }
+    @Test
+    public void twoSumCondition1(){
+        this.result=this.ts.solution(new int[]{2,7,11,15},9 );
+        Arrays.sort(this.result);
+        assertArrayEquals(new int[]{0,1},this.result);
+    }
+    @Test
+    public void twoSumCondition2(){
+        this.result=this.ts.solution(new int[]{2,7,11,15},9 );
+        Arrays.sort(this.result);
+        assertArrayEquals(new int[]{0,1},this.result);
+    }
+
+
+}
